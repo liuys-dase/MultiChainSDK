a40d4f235d52b4058110af2284e11306852f4925
liuys-dase <liuys@stu.ecnu.edu.cn>
2024-09-05 14:42:46 +0800

    update hyperchain_shutdown

--- scripts/hyperchain/hyperchain_shutdown.sh
+++ scripts/hyperchain/hyperchain_shutdown.sh
@@ -1,28 +1,20 @@
-#!/bin/sh
-# https://docs.hyperchain.cn/document/detail?type=1&id=58
+#!/bin/bash
 
-# 脚本所在路径
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Get all container IDs created using hyperchaincn/solo:v2.0.0
+container_ids=$(docker ps -a --filter "ancestor=hyperchaincn/solo:v2.0.0" --format "{{.ID}}")
 
-# 配置文件路径
-CONFIG_FILE="${SCRIPT_DIR}/../config.properties"
-
-CHAIN_TYPE="hyperchain"
-
-# 检查配置文件是否存在
-if [ -f "$CONFIG_FILE" ]; then
-  DATADIR=$(grep "^${CHAIN_TYPE}.data_dir" "$CONFIG_FILE" | cut -d'=' -f2)
-
-  echo "DATADIR=${DATADIR}"
-else
-  echo "Configuration file not found!"
-  exit 1
+# Check if any containers are found
+if [ -z "$container_ids" ]; then
+  echo "No containers found for hyperchaincn/solo:v2.0.0."
+  exit 0
 fi
 
-cd $DATADIR/trial_version
+# Stop the containers
+echo "Stopping all containers created using hyperchaincn/solo:v2.0.0..."
+docker stop $container_ids
 
-# step1. 停止组件
-./stop.sh
+# Remove the containers
+echo "Removing all containers created using hyperchaincn/solo:v2.0.0..."
+docker rm $container_ids
 
-# step2. 清理数据
-./reset.sh
+echo "All related containers have been stopped and removed."
